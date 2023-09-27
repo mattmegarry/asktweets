9b70b2c8c7687cf0b03be91736ce8d9fe16a8fa5
Matt Megarry <mattmegarry@gmail.com>
2023-09-27 15:21:52 +0100

    [setup] - MP and Party models

--- asktweets/tweets/admin.py
+++ asktweets/tweets/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MP, Party 
+
+admin.site.register(MP)
+admin.site.register(Party)
--- asktweets/tweets/models.py
+++ asktweets/tweets/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Party(models.Model):
+    name = models.CharField(max_length=255, unique=True, blank=False, null=False)
+
+    class Meta:
+        verbose_name_plural = "Parties"
+
+    def __str__(self):
+        return self.name
+
+class MP(models.Model):
+    name = models.CharField(max_length=600, blank=False, null=False)
+    party = models.ForeignKey(Party, on_delete=models.SET_NULL, null=True)
+    constituency = models.CharField(max_length=600)
+    twitter_handle = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "MPs"
+
+    def __str__(self):
+        return self.name
