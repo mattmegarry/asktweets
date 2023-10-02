c55a9d61d7be2a84b29f148ca6184aff2e29c8b0
Matt Megarry <mattmegarry@gmail.com>
2023-10-02 12:14:27 +0100

    [feature] - admin user can search for an MP and see all MPs in one list

--- asktweets/tweets/admin.py
+++ asktweets/tweets/admin.py
@@ -5,6 +5,8 @@ from .models import MP, Party, Tweet, ClaudeAPIKey, ApifyAPIKey
 class MPAdmin(admin.ModelAdmin):
     list_display = ('name', 'twitter_handle', 'tweets_last_scraped_days_ago')
     readonly_fields = ('tweets_last_scraped',)
+    list_per_page = 700
+    search_fields = ('name',)
 
 admin.site.register(MP, MPAdmin)
 admin.site.register(Party)
