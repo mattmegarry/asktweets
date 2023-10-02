a2d764741d9ba689b90e242bd7e1a18fe9e75b35
Matt Megarry <mattmegarry@gmail.com>
2023-10-02 10:55:52 +0100

    [feature] - admin user can create and edit apify api key

--- asktweets/tweets/admin.py
+++ asktweets/tweets/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import MP, Party, Tweet, ClaudeAPIKey
+from .models import MP, Party, Tweet, ClaudeAPIKey, ApifyAPIKey
 
 admin.site.register(MP)
 admin.site.register(Party)
 admin.site.register(Tweet)
 admin.site.register(ClaudeAPIKey)
+admin.site.register(ApifyAPIKey)
--- asktweets/tweets/models.py
+++ asktweets/tweets/models.py
@@ -31,7 +31,7 @@ class Tweet(models.Model):
         return self.tweet_text[:50]
 
 class ClaudeAPIKey(models.Model):
-    value = models.CharField(max_length=1000, unique=True, blank=False, null=False)
+    value = models.CharField(max_length=1000, unique=True, blank=False, null=True, default=None)
 
     class Meta:
         verbose_name = "Claude API Key"
@@ -39,6 +39,26 @@ class ClaudeAPIKey(models.Model):
 
     def __str__(self):
         return "Claude API Key"
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        self.__class__.objects.exclude(pk=1).delete()
+        super().save(*args, **kwargs)
+
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
+    
+class ApifyAPIKey(models.Model):
+    value = models.CharField(max_length=1000, unique=True, blank=False, null=True, default=None)
+
+    class Meta:
+        verbose_name = "Apify API Key"
+        verbose_name_plural = "Apify API Key"
+
+    def __str__(self):
+        return "Apify API Key"
 
     def save(self, *args, **kwargs):
         self.pk = 1
