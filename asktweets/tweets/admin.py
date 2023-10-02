5dd806f18c8fc9314c36424d5bc04e57572c846d
Matt Megarry <mattmegarry@gmail.com>
2023-10-02 12:36:02 +0100

    [checkpoint] - admin user can perform a placeholder custom action

--- asktweets/tweets/admin.py
+++ asktweets/tweets/admin.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 
 from .models import MP, Party, Tweet, ClaudeAPIKey, ApifyAPIKey
 
+@admin.action(description="Log these MPs' names")
+def log_name(self, request, queryset):
+        for mp in queryset:
+            print(mp.name)
+
 class MPAdmin(admin.ModelAdmin):
     list_display = ('name', 'twitter_handle', 'tweets_last_scraped_days_ago')
     readonly_fields = ('tweets_last_scraped',)
     list_per_page = 700
     search_fields = ('name',)
     ordering = ('name',)
+    actions = [log_name]
 
 admin.site.register(MP, MPAdmin)
 admin.site.register(Party)
